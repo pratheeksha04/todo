2a2b7ad4d8ae467f3bcd8ca768b8c319d63ab1d4
Pratheeksha <pratheeksha@nidec-read.com>
2024-09-16 12:23:26 +0530

    Todo App Completed

--- todo_main/views.py
+++ todo_main/views.py
@@ -6,7 +6,9 @@ from todos.models import Task
 def home(request):
     # return HttpResponse('<h1>homepage</h1>')
     tasks=Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks=Task.objects.filter(is_completed=True)
     context={
         'tasks':tasks,
+        "completed_tasks":completed_tasks
     }
     return render(request, 'home.html',context)
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.shortcuts import HttpResponse
 from .models import Task
 # Create your views here.
@@ -7,4 +7,29 @@ def addTask(request):
     task=request.POST['task']
     Task.objects.create(task=task)#from model
     # return HttpResponse('form submitted')
+    return redirect('home')
+
+def mark_as_done(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+
+def edit_task(request,pk):
+    get_task=get_object_or_404(Task,pk=pk)
+    #if get request then just load task if post then update send to server
+    if request.method=='POST':
+        new_task=request.POST['task']#input type name is task 
+        get_task.task=new_task #TASK FIELD
+        get_task.save()
+        return redirect('home')
+    else:
+        context={
+            'get_task':get_task
+        }
+        return render(request,'edit_task.html',context)
+
+def delete_task(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.delete()
     return redirect('home')
